c7a6cb434651781b51f8cadece7ec70e08a603e9
bumbleblo <bumbleblo2013@gmail.com>
2019-04-26 12:35:08 -0300

    #97 - Removing custom serial options
    Signed-off-by: bumbleblo <bumbleblo2013@gmail.com>

--- unbrake-server/src/main.go
+++ unbrake-server/src/main.go
@@ -2,23 +2,12 @@ package main
 
 import "fmt"
 import "./usb"
-import "github.com/goiiot/libserial"
-
-var (
-    baseOptions = []libserial.Option{
-        libserial.WithDataBits(8),
-        libserial.WithParity(libserial.ParityNone),
-        libserial.WithHardwareFlowControl(true),
-        libserial.WithSoftwareFlowControl(true),
-        libserial.WithStopBits(libserial.StopBitOne),
-    }
-)
 
 func main(){
 
     fmt.Println("Funcionou")
 
-    teste := usb.OpenConnection("/dev/pts/2", baseOptions...)
+    teste := usb.OpenConnection("/dev/pts/2")
 
     data := make([]byte, 1)
 
--- unbrake-server/src/write.go
+++ unbrake-server/src/write.go
@@ -1,32 +1,17 @@
 package main
 import "fmt"
 import "./usb"
-import "github.com/goiiot/libserial"
 import "strconv"
-
-
-var (
-	baseOptions = []libserial.Option{
-		libserial.WithDataBits(8),
-		libserial.WithParity(libserial.ParityNone),
-		libserial.WithHardwareFlowControl(true),
-		libserial.WithSoftwareFlowControl(true),
-        libserial.WithStopBits(libserial.StopBitOne),
-	}
-)
-
 
 func main(){
 
     fmt.Println("Funcionou")
 
-    teste := usb.OpenConnection("/dev/pts/2", baseOptions...)
+    teste := usb.OpenConnection("/dev/pts/2")
 
     fmt.Println([]byte(strconv.Itoa(10)))
+    teste.Connection.Write([]byte(" "))
 
-    for i:=0; i < 256; i++{
-        teste.Connection.Write([]byte(strconv.Itoa(i)))
-    }
     teste.Connection.Close()
 
 }
