93d3a944e60f39874da98571068109f4a6bad252
bumbleblo <bumbleblo2013@gmail.com>
2019-04-29 19:38:18 -0300

    #97 - Removing write from simulator
    Signed-off-by: bumbleblo <bumbleblo2013@gmail.com>

--- unbrake-server/src/main.go
+++ unbrake-server/src/main.go
@@ -2,19 +2,25 @@ package main
 
 import "fmt"
 import "./usb"
+import "github.com/goiiot/libserial"
 
 func main(){
 
-    fmt.Println("Funcionou")
+    fmt.Println("Lendo caracteres do terminal")
 
-    teste := usb.OpenConnection("/dev/pts/2")
+    teste := usb.OpenConnection(
+        "/dev/tty10",
+        libserial.WithBaudRate(9600),
+        libserial.WithStopBits(libserial.StopBitOne))
+
+    teste.Connection.Flush()
 
     data := make([]byte, 1)
 
     teste.Connection.Flush()
     for {
        teste.Connection.Read(data[:])
-       fmt.Println(data)
+       fmt.Printf("%s\n", data)
     }
 
     teste.Connection.Close()
